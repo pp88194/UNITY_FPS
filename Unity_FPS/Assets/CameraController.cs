0bbf5033230dfd6796f22590a3c3c818ac094a99
pp88194 <85343530+pp88194@users.noreply.github.com>
2022-01-10 09:44:07 +0900

    카메라
    카메라 완성

--- Unity_FPS/Assets/CameraController.cs
+++ Unity_FPS/Assets/CameraController.cs
@@ -4,14 +4,19 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    readonly float MIN_rotateY = -75;
-    readonly float MAX_rotateY = 70;
+    //카메라 X축 회전 범위제한 값
+    readonly float FPS_MIN_rotateY = -75;
+    readonly float FPS_MAX_rotateY = 70;
+    readonly float TPS_MIN_rotateY = 15;
+    readonly float TPS_MAX_rotateY = 50;
 
     public float currentRotateX { get; private set; }
     public float currentRotateY { get; private set; }
 
-    [SerializeField] float cameraSensitivity = 1;
+    public enum CameraMode { FPS, TPS}
+    public CameraMode cameraMode;
 
+    [SerializeField] float mouseSensitivity = 1; //마우스 감도
 
     Camera mainCamera;
 
@@ -25,13 +30,51 @@ public class CameraController : MonoBehaviour
 
     private void Update()
     {
-        currentRotateX += Input.GetAxis("Mouse X") * cameraSensitivity;
-        currentRotateY = Mathf.Clamp(currentRotateY - Input.GetAxis("Mouse Y") * cameraSensitivity, MIN_rotateY, MAX_rotateY);
+        if (Input.GetKeyDown(KeyCode.T))
+            SetCameraMode(cameraMode == CameraMode.FPS ? CameraMode.TPS : CameraMode.FPS);
+        InputMouse();
     }
 
     private void FixedUpdate()
     {
-        transform.rotation = Quaternion.Euler(currentRotateX, transform.localEulerAngles.y, transform.localEulerAngles.z);
-        mainCamera.transform.localRotation = Quaternion.Euler(currentRotateY, mainCamera.transform.localEulerAngles.y, mainCamera.transform.localEulerAngles.z);
+        CameraRotate();
+    }
+
+    public void SetCameraMode(CameraMode mode)
+    {
+        if (cameraMode == mode) return; //중복처리 안되게
+        cameraMode = mode;
+        if(mode == CameraMode.FPS) mainCamera.transform.position = transform.position;
+    }
+
+    void InputMouse()
+    {
+        currentRotateX += Input.GetAxis("Mouse X") * mouseSensitivity;
+        if(cameraMode == CameraMode.FPS)
+            currentRotateY = Mathf.Clamp(currentRotateY - Input.GetAxis("Mouse Y") * mouseSensitivity, FPS_MIN_rotateY, FPS_MAX_rotateY);
+        else
+            currentRotateY = Mathf.Clamp(currentRotateY - Input.GetAxis("Mouse Y") * mouseSensitivity, TPS_MIN_rotateY, TPS_MAX_rotateY);
+    }
+    void CameraRotate()
+    {
+        //FPS
+        if (cameraMode == CameraMode.FPS)
+        {
+            transform.rotation = Quaternion.Euler(transform.localEulerAngles.x, currentRotateX, transform.localEulerAngles.z);
+            mainCamera.transform.localRotation = Quaternion.Euler(currentRotateY, mainCamera.transform.localEulerAngles.y, mainCamera.transform.localEulerAngles.z);
+        }
+        //TPS
+        else if (cameraMode == CameraMode.TPS)
+        {
+            mainCamera.transform.LookAt(transform);
+            transform.rotation = Quaternion.Euler(transform.localEulerAngles.x, currentRotateX, transform.localEulerAngles.z);
+
+            float cameraDistance = 5; //임시로 지정한 변수임
+            mainCamera.transform.position = new Vector3(
+                Mathf.Sin(currentRotateX * Mathf.Deg2Rad) * Mathf.Cos(currentRotateY * Mathf.Deg2Rad) * cameraDistance,
+                Mathf.Sin(currentRotateY * Mathf.Deg2Rad) * cameraDistance,
+                Mathf.Cos(currentRotateX * Mathf.Deg2Rad) * Mathf.Cos(currentRotateY * Mathf.Deg2Rad) * cameraDistance
+            );
+        }
     }
 }
